8d7a7dec8e43fe588ed197ee49a73aafaafa7669
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2022-01-04 10:57:10 +0700

    mass extracting script in python

---
+++ mass_extract.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import subprocess
+import sys
+
+argparser = argparse.ArgumentParser(
+    description="Mass extractor for Breath of Fire IV dialogue sections"
+)
+argparser.add_argument(
+    "source", metavar="SOURCE", type=str, help="Source folder containing the .EMI files"
+)
+argparser.add_argument(
+    "target",
+    metavar="TARGET",
+    type=str,
+    help="Output folder containing the extracted dialogues",
+)
+argparser.add_argument("-j", "--jp", "--japanese", action="store_true")
+argparser.add_argument("-v", "--verbose", action="store_true")
+args = argparser.parse_args()
+
+
+def main():
+    files = os.listdir(path=args.source)
+
+    if not os.path.exists(args.target):
+        os.mkdir(args.target)
+
+    extractor = "jpextractor.exe" if args.jp else "extractor.exe"
+
+    for i in files:
+        if args.verbose:
+            print("Extracting", i)
+        subprocess.run(
+            [
+                extractor,
+                "/".join([args.source, i]),
+                "".join([args.target, "/", i, ".OUT"]),
+            ]
+        )
+
+
+if __name__ == "__main__":
+    main()
